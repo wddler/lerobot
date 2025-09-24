bcfa46d7a0e5ac3c6730695e8b6775ccca67c3e1
Denis <wddler@gmail.com>
2025-09-24 17:02:19 +0200

    update demo commands and script

--- demo/teleop_demo.sh
+++ demo/teleop_demo.sh
@@ -15,7 +15,7 @@ python -c "import lerobot"  # This will fail if the package isn't installed
 
 # Change directory and run the Python script
 cd "$HOME/lerobot"
-exec python -m lerobot.teleoperate \
+exec lerobot-teleoperate \
     --robot.type=so100_follower \
     --robot.port=/dev/ttyACM0 \
     --robot.id=my_awesome_follower_arm \
@@ -23,6 +23,3 @@ exec python -m lerobot.teleoperate \
     --teleop.port=/dev/ttyACM1 \
     --teleop.id=my_awesome_leader_arm \
     --display_data=false
- 
-
-
